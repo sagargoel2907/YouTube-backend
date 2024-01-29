b669e69778eeafd1149b4fad788cdbfab5f26f4d
sagar_goel <sagar_goel@hcl.com>
2024-01-30 03:20:01 +0530

    created dummy controllers and routes for the functionality required

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import fs from "fs";
 import path from "path";
 import { ApiError } from "./utils/ApiError.js";
+import asyncHandler from "./utils/asyncHandler.js";
 
 const app = express();
 
@@ -20,10 +21,25 @@ app.use(cookieParser());
 
 // routers import
 import userRouter from "./routes/user.routes.js";
-import asyncHandler from "./utils/asyncHandler.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 // routes declaration
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 app.route("/api/v1/file/:fileName").get(
   asyncHandler((req, res) => {
